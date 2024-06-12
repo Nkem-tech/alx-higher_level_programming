f0a2b6376121c74197a9baef5f14ea401e1aa902
Okechukwu Nkemdirim <artfreek57367@gmail.com>
2024-06-11 20:20:19 -0700

    commit 1

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+--  script that displays the average temperature (Fahrenheit) by city ordered by temperature
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
